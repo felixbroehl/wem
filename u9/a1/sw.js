c9836c3196e47b8188a3638e8f6d5d74b895b04f
Felix Bröhl <felix@broehl.dev>
2021-12-12 16:00:38 +0100

    Add exercise 9

---
+++ u9/a1/sw.js
@@ -0,0 +1,32 @@
+const cacheName = 'www-navigator-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll([
+                'assets/a3_contents.json',
+                'main.js',
+                'index.html',
+                'manifest.json',
+                'https://cdn.skypack.dev/lit',
+                'https://cdn.skypack.dev/lit/directives/class-map.js'
+            ])
+        })
+    )
+})
+
+self.addEventListener('fetch', event => { // From https://developer.mozilla.org/en-US/docs/Web/API/FetchEvent
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(async function() {
+        const cache = await caches.open(cacheName);
+        const cachedResponse = await cache.match(event.request);
+
+        if (cachedResponse) {
+            event.waitUntil(cache.add(event.request));
+            return cachedResponse;
+        }
+
+        return fetch(event.request);
+    }());
+});
