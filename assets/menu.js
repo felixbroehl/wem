0038d06df0b7936626215609d841b506502bdfae
Felix Bröhl <felix@broehl.dev>
2022-02-25 18:48:09 +0100

    Fix ServiceWorker install and remove old files

--- assets/menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('readystatechange', () => {
-    if (document.readyState === 'complete') {
-        const burgerButton = document.querySelector('#burger-button > div');
-        const nav = document.querySelector('nav');
-        burgerButton.addEventListener('click', () => {
-            nav.classList.toggle('active');
-        })
-    }
-})
