60a06044ad0efda326c7d844410e597855ead3d5
Felix Bröhl <felix@broehl.dev>
2022-02-25 20:17:19 +0100

    Update generation of url from chapter names

--- components/utils.js
+++ components/utils.js
@@ -1,9 +1,10 @@
 const nameToUrlPart = (name) => {
     return name.toLowerCase()
+        .replace(/[#?&/=+.*'{}()%$§"!;,:´`]+/g, '')
         .replace(/ /g, '-')
+        .replace(/ä/g, 'ae')
         .replace(/ü/g, 'ue')
-        .replace(/ö/g, 'ae')
-        .replace(/ö/g, 'ae')
-        .replace(/[#.]/g, '_');
+        .replace(/ö/g, 'oe')
+        .replace(/ß/g, 'ss');
 }
 export {nameToUrlPart};
