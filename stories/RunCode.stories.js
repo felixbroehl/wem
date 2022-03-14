c2e2cece61f0ace28a0482aeca56b5ec694713d7
Felix Bröhl <felix@broehl.dev>
2022-03-14 13:56:21 +0100

    Minor fix for storybook

--- stories/RunCode.stories.js
+++ stories/RunCode.stories.js
@@ -17,7 +17,7 @@ const Template = ({ label, ...args }) => {
   // You can either use a function to create DOM elements or use a plain html string!
   return `<script type="module" src="https://felixbroehl.github.io/wem/components/code-preview.js"></script><!--Because external imports in JS cannot be processed with storybook/webpack-->
     <script type="module" src="https://felixbroehl.github.io/wem/components/run-code.js"></script><!--Because external imports in JS cannot be processed with storybook/webpack-->
-    <run-code ${args.htmlSrc?'htmlSrc="' + args.htmlSrc + '"':''} ${args.jsSrc?'jsSrc="' + args.jsSrc + '"':''} ${args.cssSrc?'cssSrc="' + args.cssSrc + '"':''} ${args.size?'size="' + args.size + '"':''} ${args.jsType?'jsType="' + args.jsType + '"':''} ${args.hideCode?'hideCode="' + args.hideCode + '"':''}></run-code>
+    <run-code ${args.htmlSrc?'htmlSrc="' + args.htmlSrc + '"':''} ${args.jsSrc?'jsSrc="' + args.jsSrc + '"':''} ${args.cssSrc?'cssSrc="' + args.cssSrc + '"':''} ${args.size?'size="' + args.size + '"':''} ${args.jsType?'jsType="' + args.jsType + '"':''} ${args.hideCode=='true'?'hideCode="' + args.hideCode + '"':''}></run-code>
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap');
       run-code {
